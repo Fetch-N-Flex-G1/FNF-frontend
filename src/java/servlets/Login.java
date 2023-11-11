3e0a2e84b52a3319219c02760ccdffe75c7254e1
Anirban Dutta <duttashaan102@gmail.com>
2023-11-11 15:38:34 +0530

    globalise the sql connection for Login.java

--- src/java/servlets/Login.java
+++ src/java/servlets/Login.java
@@ -89,16 +89,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import oracle.jdbc.OracleConnection;
+
+import helperclasses.SQLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Login extends HttpServlet {
-    private static final String SELECT_QUERY = "SELECT EMAIL, PASSWORD FROM user_creds WHERE EMAIL=? AND PASSWORD=?";//ekhane password tao kano fetch krchis?
-    //are fetch na bolchi where e kano deachis? 
-    //pass ta ke simply fetch kkre user er enter kra pass er sathe compare o toh krte paris tle bar bar db er sthe
-    //connect kre query run krte hbe na tle backend e chap porbe
-    //java te code likhte paris na?
-    //hell even better! password ta pass kre de login.jsp ke tale okhanei red kre bole debe je bhul ache
-    //nahole nije bol pass bhul hole ki kre janabi? 
-    //naa page ta change joe jacche je same page e tjakte hbe je
+    private static final String SELECT_QUERY = "SELECT EMAIL, PASSWORD FROM user_creds WHERE EMAIL=? AND PASSWORD=?";
     
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -112,25 +110,38 @@ public class Login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter pw = response.getWriter();
-        String userEmail = request.getParameter("SEMAIL");
-        String userPassword = request.getParameter("SPASS");
-
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1522:free", "c##FANDf", "database");
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY)) {
-
+        try {
+            response.setContentType("text/html");
+            PrintWriter pw = response.getWriter();
+            String userEmail = request.getParameter("SEMAIL");
+            String userPassword = request.getParameter("SPASS");
+            
+            SQLConnection sqlcon = new SQLConnection();
+            
+            OracleConnection connection = null;
+            try {
+                connection = sqlcon.connect();
+            } catch (SQLException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            PreparedStatement preparedStatement = null;
+            try {
+                preparedStatement = connection.prepareStatement(SELECT_QUERY);
+            } catch (SQLException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             preparedStatement.setString(1, userEmail);
             preparedStatement.setString(2, userPassword);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
+            ResultSet resultSet = preparedStatement.executeQuery(); 
+            
             if (resultSet.next()) {
-                    // Redirect to home.html
-                    response.sendRedirect("../../../Fetch-N-Flex/Pages/home.html");
-
-                    pw.println("</body></html>");
-
+                // Redirect to home.html
+                response.sendRedirect("../../../Fetch-N-Flex/Pages/home.html");
+                
+                pw.println("</body></html>");
+                
             } else {
                 pw.println("<html><body>");
                 pw.println("<center><h3>Invalid username/password. Enter Correct username/password</h3></center>");
@@ -138,10 +149,9 @@ public class Login extends HttpServlet {
                 rd2.include(request, response);
                 pw.println("</body></html>");
             }
-        } 
-        catch (SQLException e) 
-        {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
+        } 
+        
 }
