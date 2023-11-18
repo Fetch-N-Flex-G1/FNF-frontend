e2f40f530948ad273cc97b0b4e79dca772edc909
Anirban Dutta <duttashaan102@gmail.com>
2023-11-18 20:46:12 +0530

    complying with oracle 10g specifications

--- src/java/helperclasses/SQLConnection.java
+++ src/java/helperclasses/SQLConnection.java
@@ -4,7 +4,9 @@
  */
 package helperclasses;
 
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import oracle.jdbc.OracleConnection;
@@ -21,31 +23,39 @@ public class SQLConnection {
             return oconn;
     }
     
-    public void runQuery(String query) throws SQLException{
+    public void runQuery(String table_name, String query) throws SQLException{
             Statement statement = oconn.createStatement();
-            statement.execute(query);
+            DatabaseMetaData metaData = oconn.getMetaData();
+            ResultSet tables = metaData.getTables(null, null, table_name, null);
+
+            if (tables.next()) {
+                System.out.println("Table " + table_name + " exists.");
+            } else {
+                System.out.println("Table " + table_name + " does not exist.");
+                statement.execute(query);
+            }
     }
     public void createUserCreds() throws SQLException{
-        runQuery("create table if not exists user_creds(email varchar2(255) PRIMARY KEY, password varchar2(255))");
+        runQuery("user_creds","create table user_creds(email varchar2(255) PRIMARY KEY, password varchar2(255))");
     }
 
     public void createContacts() throws SQLException{
-        runQuery("create table if not exists contacts(NAME varchar2(100), PHONE_NUMBER number(20), EMAIL varchar2(100))");
+        runQuery("contacts","create table contacts(NAME varchar2(100), PHONE_NUMBER number(20), EMAIL varchar2(100))");
     }
     
     public void createUserDetails() throws SQLException{
-        runQuery("create table if not exists user_details(f_name varchar2(100), l_name varchar2(100), email varchar2(255), ph_no number(20),address varchar2(400), gender varchar2(30),FOREIGN KEY (email) REFERENCES user_creds(email))");
+        runQuery("user_details","create table user_details(f_name varchar2(100), l_name varchar2(100), email varchar2(255), ph_no number(20),address varchar2(400), gender varchar2(30),FOREIGN KEY (email) REFERENCES user_creds(email))");
     }
     public void createPetDetails() throws SQLException{
-        runQuery("create table if not exists pet_details(Pet_Name varchar2(100), Owner_Name varchar2(100), Weight number(20), Height number(20),Breed varchar2(400), Age number(30),Gender varchar2(30))");
+        runQuery("pet_details","create table pet_details(Pet_Name varchar2(100), Owner_Name varchar2(100), Weight number(20), Height number(20),Breed varchar2(400), Age number(30),Gender varchar2(30))");
     }
     
     public void createFeedback() throws SQLException{
-        runQuery("create table if not exists feedback(NAME varchar2(200), EMAIL varchar2(200), FEEDBACK varchar2(1000))");
+        runQuery("feedback","create table feedback(NAME varchar2(200), EMAIL varchar2(200), FEEDBACK varchar2(1000))");
     }
 
     public void createGetInTouch() throws SQLException{
-        runQuery("create table if not exists GetInTouch(NAME varchar2(200), EMAIL varchar2(200), MESSAGE varchar2(1000))");
+        runQuery("get_in_touch","create table get_in_touch(NAME varchar2(200), EMAIL varchar2(200), MESSAGE varchar2(1000))");
     }
 
     public void quit() throws SQLException{
